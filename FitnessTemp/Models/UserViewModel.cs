9878187967d72769bbf57ec2a906a23679c34f92
MrMultiRel <71727874+MrMultiRel@users.noreply.github.com>
2023-01-09 11:44:43 +0300

    Delete FitnessTemp directory

--- FitnessTemp/Models/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FitnessTemp.Models
-{
-    public class UserViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Age { get; set; }
-    }
-}
