d984b5d096f0b4bc145f7e2ad325c91858aae307
MrMultiRel <71727874+MrMultiRel@users.noreply.github.com>
2023-01-09 11:50:16 +0300

    Add files via upload

---
+++ BLL/DTO/ExercisesDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BLL.DTO
+{
+    public class ExercisesDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Instraction { get; set; }
+    }
+}
