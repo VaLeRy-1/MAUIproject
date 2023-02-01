e06501c740cae0b4e1ef8c21304354fa96e3bfac
MrMultiRel <71727874+MrMultiRel@users.noreply.github.com>
2023-02-01 12:23:52 +0300

    Delete Models directory

--- Models/CurrentUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PL.Models
-{
-    public static class CurrentUser
-    {
-        public static string Name { get; set; }
-        public static int Age { get; set; }
-    }
-}
--- Models/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PL.Models
-{
-    public class UserViewModel
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Password { get; set; }
-    }
-}
