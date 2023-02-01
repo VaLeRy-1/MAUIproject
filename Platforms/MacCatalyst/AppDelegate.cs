1578aa085efe5cd9b0dd1e5e564292ee7fe31829
MrMultiRel <71727874+MrMultiRel@users.noreply.github.com>
2023-02-01 12:23:22 +0300

    Delete Platforms directory

--- Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace PL;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
